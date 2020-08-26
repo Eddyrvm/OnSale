4a28c544a094f904c98d3d19295d71ab8b38d199
Eddy Vera Mejia <nayo_ec@outlook.com>
2020-08-26 12:37:05 -0500

    IMAGE IN CATEGORY

--- OnSale.Common/Entities/Category.cs
+++ OnSale.Common/Entities/Category.cs
@@ -19,8 +19,8 @@ namespace OnSale.Common.Entities
         //TODO: Pending to put the correct paths
         [Display(Name = "Image")]
         public string ImageFullPath => ImageId == Guid.Empty
-            ? $"https://localhost:44390/images/noimage.png"
-            : $"https://onsale.blob.core.windows.net/categories/{ImageId}";
+            ? $"https://localhost:44349/images/noimage.png"
+            : $"https://onsaleeddy.blob.core.windows.net/categories/{ImageId}";
     }
 
 }
